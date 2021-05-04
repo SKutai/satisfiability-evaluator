63c348072ac8479b3e417aeae34f92b11a8b4199
Seraiah Kutai <SeKutai@Ursinus.edu>
2021-05-04 14:20:23 -0400

    safe from replit

--- boolean.py
+++ boolean.py
@@ -1,10 +1,22 @@
+'''
+Satisfiability Evaluator
+	@author William Clift
+	@author Seraiah Kutai
+	@date 5 May 2020
+
+	Run:
+		python boolean.py
+
+'''
+
 import re
 
 class BooleanScanner:
 
-    tokes = ['[a-zA-Z][a-zA-Z0-9]*', ':=', '(', '0', '1', 'AND', 'OR', 'NOT', 'XOR', ')']
+    regexs = ['[a-zA-Z][a-zA-Z0-9]*', ':=', '(', '0', '1', 'AND', 'OR', 'NOT', 'XOR', ')']
     types = ['vari', 'asgn', 'lpar', 'false', 'true', 'a', 'o', 'n', 'x', 'rpar']
     lines = []
+    pairs = []
 
     def __init__(self, source):
         self.getInput(source)
@@ -13,10 +25,13 @@ class BooleanScanner:
         s = 'input.txt'
         with open(s) as f:
             self.lines = f.readlines()
-            print('read: ' + self.lines)
+            print('read: ', self.lines)
 
     def scanInput(self):
         for line in self.lines:
             words = line.split()
             for word in words:
-
+                for regex in regexs:
+                match = re.match(regex, word)
+                if(match != None):
+                  self.pairs.append([types[regexs.index(regex)], word])
