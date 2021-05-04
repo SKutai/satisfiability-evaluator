e5b751e71017bcdf3a09a8857708bca66cc3e044
Seraiah Kutai <SeKutai@Ursinus.edu>
2021-05-04 13:28:44 -0400

    reads input working on breaking it up

--- boolean.py
+++ boolean.py
@@ -1,4 +1,22 @@
 import re
 
-tokes = ['[a-zA-Z][a-zA-Z0-9]*', '0', '1', ':=', 'AND', 'OR', 'NOT', 'XOR', '(', ')']
+class BooleanScanner:
 
+    tokes = ['[a-zA-Z][a-zA-Z0-9]*', ':=', '(', '0', '1', 'AND', 'OR', 'NOT', 'XOR', ')']
+    types = ['vari', 'asgn', 'lpar', 'false', 'true', 'a', 'o', 'n', 'x', 'rpar']
+    lines = []
+
+    def __init__(self, source):
+        self.getInput(source)
+
+    def getInput(self, s):
+        s = 'input.txt'
+        with open(s) as f:
+            self.lines = f.readlines()
+            print('read: ' + self.lines)
+
+    def scanInput(self):
+        for line in self.lines:
+            words = line.split()
+            for word in words:
+                
